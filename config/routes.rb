757e5ddcdedfac7eaf2c80e8769fe97cd8dc192e
Stefania Ella Centea <stefiinusj@gmail.com>
2021-08-31 11:17:53 +0100

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
     resources :companies, only: %i[new create]
   end
   root to: 'pages#home'
-  get '/profile', to: 'pages#profile'
-  get '/package', to: 'pages#package'
-  resources :perks do
-    resources :user_perks
-    resources :reviews
+  get '/profile', to: 'users#profile'
+  get '/package', to: 'users#package'
+  resources :perks, only: %i[show index] do
+    resources :user_perks, only: %i[create]
+    resources :reviews, only: %i[create]
   end
 end
