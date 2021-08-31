6a791744013358c8781e8e4443721f9bcbaccc37
Khuthadzo Masindi <khuthadzo.masindi@gmail.com>
2021-08-31 17:02:36 +0100

    add perks to account

--- app/controllers/perks_controller.rb
+++ app/controllers/perks_controller.rb
@@ -1,8 +1,17 @@
 class PerksController < ApplicationController
 
+  before_action :set_perk, only: [:show, :edit, :update, :destroy, :chef]
+
   def show
-    @perk = Perk.find(params[:id])
   end
 
+  private
 
+  def perks_params
+    params.require(:perk).permit(:name, :token_cost, :description, :perk_pic, :merchants)
+  end
+
+  def set_perk
+    @perk = Perk.find(params[:id])
+  end
 end
