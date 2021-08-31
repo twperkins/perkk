da8fad035109096f5bce07970e6f1b7e47a14a16
Stefania Ella Centea <stefiinusj@gmail.com>
2021-08-31 15:08:30 +0100

    seed2

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
   has_many :users
   has_one_attached :logo_pic
-  validates :name, :total_token, :head_count, :subscription_start, :subscription_end, presence: true
+  validates :name, :total_token, :head_count, :subscription_end, :subscription_start, presence: true
   validates :name, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,8 +24,8 @@ company = Company.create!(
   name: "Facebook",
   total_token: 20_000,
   head_count: 20,
-  subscription_start: DateTime.parse("09/01/2009 17:00"),
-  subscription_end: DateTime.parse("09/01/2010 19:00")
+  subscription_start: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
+  subscription_end: Faker::Date.between(from: '2014-09-23', to: '2014-09-25')
 )
 file = URI.open('https://source.unsplash.com/800x600/?company')
 company.logo_pic.attach(io: file, filename: 'test.png', content_type: 'image/png')
@@ -53,7 +53,9 @@ puts "creating the rest of the users.."
     password: Faker::Internet.password,
     name: Faker::Internet.user,
     company_id: company.id,
-    tokens: 1000
+    tokens: 1000,
+    location: "London",
+    unique_code: Faker::Barcode.ean
   )
   file = URI.open('https://source.unsplash.com/800x600/?person')
   user.profile_pic.attach(io: file, filename: 'test.png', content_type: 'image/png')
@@ -62,13 +64,13 @@ end
 
 puts "creating amazing perks.."
 # creating perk 1
-Perk.create!(
+perk = Perk.create!(
   name: "Five Free Friday Rides",
   description: "This perk offers you 5 free riders on 5 Fridays!",
   location: "United Kingdom",
   token_cost: 50,
-  start_date: DateTime.parse("09/01/2009 17:00"),
-  end_date: DateTime.parse("09/01/2010 19:00"),
+  start_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
+  end_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
   category: "travel",
   merchants: "Uber"
 )
@@ -76,13 +78,13 @@ file = URI.open('https://source.unsplash.com/800x600/?uber')
 perk.perk_pic.attach(io: file, filename: 'test.png', content_type: 'image/png')
 
 # creating perk 2
-Perk.create!(
+perk = Perk.create!(
   name: "Bounce: the home of ping-pong",
   description: "Using this perk will allow you to have 10 free games in any bounce location in Lonon. ",
   location: "London",
   token_cost: 100,
-  start_date: DateTime.parse("09/01/2009 17:00"),
-  end_date: DateTime.parse("09/01/2010 19:00"),
+  start_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
+  end_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
   category: "social",
   merchants: "Bounce"
 )
@@ -90,13 +92,13 @@ file = URI.open('https://source.unsplash.com/800x600/?bounce')
 perk.perk_pic.attach(io: file, filename: 'test.png', content_type: 'image/png')
 
 # creating perk 3
-Perk.create!(
+perk = Perk.create!(
   name: "Wings",
   description: "This perk offers you 50% off on any Ryan flights booked on Thursdays.",
   location: "London",
   token_cost: 150,
-  start_date: DateTime.parse("09/01/2009 17:00"),
-  end_date: DateTime.parse("09/01/2010 19:00"),
+  start_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
+  end_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
   category: "travel",
   merchants: "Ryan Air"
 )
@@ -104,15 +106,15 @@ file = URI.open('https://source.unsplash.com/800x600/?ryanair')
 perk.perk_pic.attach(io: file, filename: 'test.png', content_type: 'image/png')
 
 # creating perk 4
-Perk.create!(
+perk = Perk.create!(
   name: "City gardens",
   description: "Roam free and explore the wonderful gardens that London has to offer. This perk will allow you
   to get 10 entries in any gardens you decide to visit in London (*excludes Chelsea flower show and **only available
   between March and September.)",
   location: "London",
   token_cost: 200,
-  start_date: DateTime.parse("09/03/2009 17:00"),
-  end_date: DateTime.parse("09/09/2010 19:00"),
+  start_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
+  end_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
   category: "social",
   merchants: "London Council"
 )
@@ -120,14 +122,14 @@ file = URI.open('https://source.unsplash.com/800x600/?garden')
 perk.perk_pic.attach(io: file, filename: 'test.png', content_type: 'image/png')
 
 # creating perk 5
-Perk.create!(
+perk = Perk.create!(
   name: "Pet Sitters",
   description: "Worried about your pet because too busy? Do you have an emergency? Pet Sitters offers 20 hours of free
   pet sitting. Once this hours have been used you can continue using the service with a 20% discount.",
   location: "United Kingdom",
   token_cost: 250,
-  start_date: DateTime.parse("09/01/2009 17:00"),
-  end_date: DateTime.parse("09/01/2010 19:00"),
+  start_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
+  end_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
   category: "pet",
   merchants: "Bark"
 )
