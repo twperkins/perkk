f7bc8672ee9127445dde5e9de34cd02eed798132
Myles Egan <mylesegan95@gmail.com>
2021-09-01 14:11:00 +0100

    review seeds

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,14 @@
 class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @perk = Perk.find(params[:perk_id])
+    @review.perk = @perk
+    @review.save
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
 end
