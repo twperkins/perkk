44a3e309e3654b90327bbb95a2e1e6e1b812503d
Khuthadzo Masindi <khuthadzo.masindi@gmail.com>
2021-09-03 15:18:03 +0100

    manage perks working for both add and remove

--- app/controllers/user_perks_controller.rb
+++ app/controllers/user_perks_controller.rb
@@ -6,14 +6,14 @@ class UserPerksController < ApplicationController
     @user_perk = UserPerk.new
     @user_perk.perk_id = @perk.id
     if request.headers["Content-Type"] == "application/json"
-      @user_perk.user_id = params[:user_id]
+      @user_perk.user = current_user
       if @user_perk.save
         render json: @user_perk
       else
         render json: {message: "failed to save"}
       end
     else
-      @user_perk.user_id = current_user.id
+      @user_perk.user = current_user
       if @user_perk.save
         redirect_to package_path
         # redirect_to profile_path
@@ -27,9 +27,9 @@ class UserPerksController < ApplicationController
     if request.headers["Content-Type"] == "application/json"
       @user_perk = UserPerk.find(params[:id])
       p @user_perk
-      user_perk.destroy
+      @user_perk.destroy
       # @user_perk.user_id = params[:user_id]
-        render json: {message: "user_perk deleted"}
+      render json: {message: "user_perk deleted"}
     else
       # @user_perk = UserPerk.find(params[:id])
       # @user_perk.destroy
