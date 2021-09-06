705680844670b0a849324da176dcb3e8b6cebfd5
Stefania Ella Centea <stefiinusj@gmail.com>
2021-09-06 12:33:13 +0100

    favourite method added

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -5,9 +5,14 @@ class FavouritesController < ApplicationController
     @perk = Perk.find(params[:perk_id])
     @favourite.perk = @perk
     if @favourite.save
-      redirect_to package_path
+      redirect_to package_path unless current_page?('/package')
     else
       redirect_to profile_path
     end
+  end
+
+  def destroy
+    @favourite = Favourite.find(params[:id])
+    @favourite.destroy
   end
 end
