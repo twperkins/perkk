f1c58cb47d2a395547d73544cb5d506ee7df35fc
Stefania Ella Centea <stefiinusj@gmail.com>
2021-09-06 16:03:54 +0100

    favourite-updated

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,11 +1,16 @@
 class FavouritesController < ApplicationController
+
+  # def current_page?
+
+  # end
+
   def create
     @favourite = Favourite.new
     @favourite.user = current_user
     @perk = Perk.find(params[:perk_id])
     @favourite.perk = @perk
     if @favourite.save
-      redirect_to package_path unless current_page?('/package')
+      # redirect_to package_path unless current_page?(controller: 'users', action: 'package')
     else
       redirect_to profile_path
     end
