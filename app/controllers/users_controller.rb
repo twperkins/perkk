4808842593a66e1ae640cb0e4c7ceaf1505400a2
Myles Egan <mylesegan95@gmail.com>
2021-09-07 14:58:58 +0100

    a few fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,26 +25,23 @@ class UsersController < ApplicationController
     @recommended.each do |perk|
       @unowned_perks << perk if @owned_perks.exclude?(perk) && @unowned_perks.exclude?(perk)
     end
-      @total_perks = 0
-      @users_perks = UserPerk.where(user: current_user)
-      @users_perks.each { |user_perk| @total_perks += user_perk.perk.token_cost }
-      current_user.tokens_used = @total_perks
-      current_user.save(validate: false)
-      @available_perks = current_user.token_allowance - @total_perks
+    @total_perks = 0
+    @users_perks = UserPerk.where(user: current_user)
+    @users_perks.each { |user_perk| @total_perks += user_perk.perk.token_cost }
+    current_user.tokens_used = @total_perks
+    current_user.save(validate: false)
+    @available_perks = current_user.token_allowance - @total_perks
 
-      respond_with total_perks: @total_perks, available_perks: @available_perks #,  @available_perks
-
+    respond_with total_perks: @total_perks, available_perks: @available_perks # , @available_perks
   end
-
 
   private
 
   def favourites
-     @favourites = current_user.favourites.map do |favourite|
+    @favourites = current_user.favourites.map do |favourite|
       favourite.perk
-     end
+    end
   end
-
 
   def overlay_calcs
     current_user_perks = current_user.perks
